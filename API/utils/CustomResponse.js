1f593ca9f128574cdce2ae7c5ac5c57ec436666c
vakson-cpu <vakson12@gmail.com>
2023-03-20 01:39:10 +0100

    Create custom response

---
+++ API/utils/CustomResponse.js
@@ -0,0 +1,9 @@
+class CustomResponse{
+    constructor(data,message,succeeded){
+        this.data=data;
+        this.message=message;
+        this.succeeded=succeeded;
+    }
+}
+
+module.exports=CustomResponse;
